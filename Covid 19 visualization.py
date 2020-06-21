df8e0aeeb8cdb5aeb15ada1ecc4617a77b864a97
P <p@Ps-MacBook-Pro.local>
2020-06-21 11:55:21 +0545

    removed countries fetch and added new cases

--- Covid 19 visualization.py
+++ Covid 19 visualization.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# WHO COVID 19 data
+# WHO COVID 19 data visualization using matplotlib
 
 # In[1]:
 
@@ -9,24 +9,44 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.axes.Axes as ax
+import statsmodels.api as sm
 import seaborn as sns
 
 sns.set()
 
+# In[2]:
 
-# In[10]:
 
+fullData = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/full_data.csv")
+fullData.head()
 
-data = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/full_data.csv")
-countries = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/locations.csv")
-for country in countries.location:
-    plt.suptitle(country)
+dataGroupedByLocation = dict(fullData.groupby("location").groups)
+
+for location in dataGroupedByLocation:
+    # Total Cases
+    totalCases = list(dict(fullData[fullData.location.str.contains(location)]["total_cases"]).values())
+    dates = list(dict(fullData[fullData.location.str.contains(location)]["date"]).values())
+    totalCasesByDate = dict(zip(dates, totalCases))
+    xAxisData = dates[0:len(dates):int(len(dates) / 20)]
+
+    fig, ax = plt.subplots()
+    ax.plot(totalCases)
+    plt.suptitle(location)
+    ax.set_title("TOTAL CASES")
     plt.xlabel("dates")
-    plt.ylabel("total cases")
-    total_cases = list(dict(data[data.location.str.contains(country)]["total_cases"]).values())
-    dates = list(dict(data[data.location.str.contains(country)]["date"]).values())
-    totalCasesByDate = dict(zip(dates, total_cases))
-    xAxisPoints = list(totalCasesByDate.keys())[0:len(totalCasesByDate):15]
-    fig, ax = plt.subplots() 
-    ax.set_xticklabels(xAxisPoints, rotation=45)
-    ax.plot(list(totalCasesByDate.values()))
+    plt.ylabel("population")
+    ax.set_xticklabels(xAxisData, rotation=45)
+
+    # New Cases and Death Count
+    newCases = list(dict(fullData[fullData.location.str.contains(location)]["new_cases"]).values())
+    newDeaths = list(dict(fullData[fullData.location.str.contains(location)]["new_deaths"]).values())
+
+    fig, ax = plt.subplots()
+    ax.plot(newCases, label="new cases")
+    ax.plot(newDeaths, label="deaths")
+    plt.suptitle(location)
+    ax.set_title("New cases and deaths")
+    plt.xlabel("dates")
+    plt.ylabel("population")
+    ax.set_xticklabels(xAxisData, rotation=45)
