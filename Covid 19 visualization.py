671afde880384e6a3d1c79beca7a93f0a86823fb
P <p@Ps-MacBook-Pro.local>
2020-06-27 13:00:54 +0545

    removed loop

--- Covid 19 visualization.py
+++ Covid 19 visualization.py
@@ -17,36 +17,35 @@ sns.set()
 
 # In[2]:
 
-
 fullData = pd.read_csv("https://covid.ourworldindata.org/data/ecdc/full_data.csv")
 fullData.head()
 
-dataGroupedByLocation = dict(fullData.groupby("location").groups)
-
-for location in dataGroupedByLocation:
-    # Total Cases
+def plotCountries(location):
+    #Total Cases
     totalCases = list(dict(fullData[fullData.location.str.contains(location)]["total_cases"]).values())
-    dates = list(dict(fullData[fullData.location.str.contains(location)]["date"]).values())
-    xAxisData = dates[0:len(dates): int(len(dates) / 7)]
-
+    dates = list(dict(fullData[fullData.location.str.contains(location)]["date"]).values())   
+         
+    xAxisData = dates[0:len(dates): int(len(dates)/ 7) if dates else 30 ]
     fig, ax = plt.subplots()
     ax.plot(totalCases)
     plt.suptitle(location)
     ax.set_title("TOTAL CASES")
     plt.xlabel("dates")
     plt.ylabel("population")
-    ax.set_xticklabels(xAxisData, rotation=45)
+    ax.set_xticklabels(xAxisData, rotation = 45)
 
-    # New Cases and Death Count
+    #New Cases and Death Count
     newCases = list(dict(fullData[fullData.location.str.contains(location)]["new_cases"]).values())
     newDeaths = list(dict(fullData[fullData.location.str.contains(location)]["new_deaths"]).values())
 
-    fig, ax = plt.subplots()
-    ax.plot(newCases, label="new cases")
-    ax.plot(newDeaths, label="deaths")
+    fig, ax = plt.subplots() 
+    ax.plot(newCases, label = "new cases")
+    ax.plot(newDeaths, label = "deaths")
     plt.legend()
     plt.suptitle(location)
     ax.set_title("New cases and deaths")
     plt.xlabel("dates")
     plt.ylabel("population")
-    ax.set_xticklabels(xAxisData, rotation=45)
+    ax.set_xticklabels(xAxisData, rotation = 45)
+        
+list(map(plotCountries, list(fullData.groupby("location").groups)))
